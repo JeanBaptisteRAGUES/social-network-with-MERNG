05484c6c40a8e824038ee08f6f486d7b559221e7
JeanBaptisteRAGUES <jeanbonduel313@gmail.com>
2022-03-17 21:05:27 +0100

    Ajout de sélectionner un post en particulier sur une page unique. Quand on supprime un post celui ci est aussi directement supprimé dans le cache pour que l'affichage de la suppression soit immédiat.

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import MenuBar from './components/MenuBar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import SinglePost from './pages/SinglePost'
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
             <Route exact path="/" element={<Home/>} />
             <Route exact path="/login" element={<Login/>} />
             <Route exact path="/register" element={<Register/>} />
+            <Route exact path="/posts/:postId" element={<SinglePost/>} />
           </Routes>
         </Container>
       </Router>
