1f418a351a32f4c45b59b7dc32485c85d6636c7e
JeanBaptisteRAGUES <jeanbonduel313@gmail.com>
2022-05-13 03:24:26 +0200

    Ajout du premier test cypress qui permet de vérifier qu'un utilisateur puisse bien se connecter. Création d'une commande qui permet de se logger directement via une requête graphQL au lieu de passer par l'UI, ce qui permet d'économiser du temps.

--- client/cypress/integration/0-MERNG/login.spec.js
+++ client/cypress/integration/0-MERNG/login.spec.js
@@ -1,5 +1,19 @@
 describe('payment', () => {
     it('user can connect to his account', () => {
-        cy.visit('http://localhost:3000/');
+        //cy.visit('http://localhost:3000/');
+        cy.visit('/');
+        /* cy.findByRole('link', {
+            name: /login/i
+        }).click();
+        cy.findByPlaceholderText(/username\.\./i).type('test01');
+        cy.findByPlaceholderText(/password\.\./i).type('123456');
+        cy.findByRole('button', {
+            name: /login/i
+        }).click(); */
+        cy.loginWithRequest('test01', '123456');
+        /* cy.findByRole('link', {
+            name: /test01/i
+        }).click(); */
+        cy.contains('Test 01').click();
     })
 })
--- client/cypress/support/commands.js
+++ client/cypress/support/commands.js
@@ -1,5 +1,76 @@
 import "@testing-library/cypress/add-commands"
 
+Cypress.Commands.add('login', (username, password) => {
+    cy.visit('/login');
+    cy.findByPlaceholderText(/username\.\./i).type(username);
+    cy.findByPlaceholderText(/password\.\./i).type(password);
+    cy.findByRole('button', {
+        name: /login/i
+    }).click();
+});
+
+Cypress.Commands.add('loginFromHome', (username, password) => {
+    cy.findByRole('link', {
+        name: /login/i
+    }).click();
+    cy.findByPlaceholderText(/username\.\./i).type(username);
+    cy.findByPlaceholderText(/password\.\./i).type(password);
+    cy.findByRole('button', {
+        name: /login/i
+    }).click();
+});
+
+/* const LOGIN_USER = gql`
+  mutation login(
+    $username: String!
+    $password: String!
+  ){
+    login(
+      username: $username
+      password: $password
+    ){
+      id email username createdAt token
+    }
+  }
+` */
+
+const LOGIN_USER = `
+  mutation {
+    login(username: "test01", password: "123456"){
+      id email username createdAt token
+    }
+  }
+`
+//TODO: rendre la requete dynamique en prenant en compte username et password
+Cypress.Commands.add('loginWithRequest', (username, password) => {
+    cy.request({
+        method: 'POST',
+        url: "http://localhost:5000/graphql",
+        body: { query : LOGIN_USER },
+    })
+    .then(res => {
+        cy.log(JSON.stringify(res));
+        cy.log(res.body.data.login.token);
+        window.localStorage.setItem('jwtToken', res.body.data.login.token);
+        cy.visit('/');
+    })
+});
+
+/* Cypress.Commands.add('loginWithRequest', (username, password) => {
+    cy.request({
+        method: 'POST',
+        url: "http://localhost:5000/graphql",
+        body: {
+            username: username,
+            email: `${username}@test.com`,
+            password: password
+        }
+    })
+    .then(res => {
+        window.localStorage.setItem('jwtToken', response.body.jwt);
+    })
+}); */
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
