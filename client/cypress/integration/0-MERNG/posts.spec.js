dc8388bdeca7b2d864bcfa6c3b6ffd6159b0fb82
JeanBaptisteRAGUES <jeanbonduel313@gmail.com>
2022-05-27 03:17:58 +0200

    Lors du test de la création d'un post, on test aussi si on peut le liker.

--- client/cypress/integration/0-MERNG/posts.spec.js
+++ client/cypress/integration/0-MERNG/posts.spec.js
@@ -1,4 +1,4 @@
-describe('post creation and deletion', () => {
+describe('Post can be created / liked / deleted', () => {
     const USERNAME = 'test01';
     const PASSWORD = '123456';
     const POST_CONTENT = 'THIS is a test post *$97616$*';
@@ -8,21 +8,46 @@ describe('post creation and deletion', () => {
         cy.loginWithRequest(USERNAME, PASSWORD);
     });
 
-    it('user can create a post and delete it', () => {
+    it('user can create a post, like it and delete it', () => {
         cy.findByRole('textbox').type(POST_CONTENT);
         cy.findByRole('button', {
             name: /valider/i
         }).click();
 
-        //Find the post by its content
-        const post = cy.findByText(POST_CONTENT).parent().parent();
+        //Find the post by its content and ..
+
+        //Check there is no like
+        cy.findByText(POST_CONTENT).parent().parent()
+        .should('be.visible')
+        .children().eq(1)
+        .children().eq(0)
+        .children().eq(1)
+        .should('have.text', '0');
         
+        //Like the post
+        cy.findByText(POST_CONTENT).parent().parent()
+        .should('be.visible')
+        .children().eq(1).children().eq(0).children().eq(0).click();
+
+        //Check if the like shows up
+        cy.findByText(POST_CONTENT).parent().parent()
+        .should('be.visible')
+        .children().eq(1)
+        .children().eq(0)
+        .children().eq(1)
+        .should('have.text', '1');
+
         //Find the delete btn and click it
-        post.children().eq(1).children().eq(2).click();
-        
+        cy.findByText(POST_CONTENT).parent().parent()
+        .should('be.visible')
+        .children().eq(1).children().eq(2).click();
+
         cy.findByRole('button', {
             name: /ok/i
-        }).click();
+        })
+        .should('be.visible')
+        .click();
+
         cy.findByText(POST_CONTENT).should('not.exist');
     });
 })
